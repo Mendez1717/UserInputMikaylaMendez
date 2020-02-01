9cf7dbc9c6bfd8132189046b8f728fce28968d88
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-01-31 23:43:16 -0500

    Changes made for user input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace UserInputMikaylaMendez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi, this is Mikayla Mendez");
+
+            string username;
+            Console.Write("Please enter your name");
+            username = Console.ReadLine();
+
+            //Ask user to enter value 
+            string greeting = "Are you ready to begin?";
+            string nextgreeting = "";
+
+            if (greeting == "Are you ready to begin?")
+            {
+                nextgreeting = "Hit enter to begin";
+            }
+            else if (greeting == "Do you need assistance?")
+            {
+                nextgreeting = "Do not continue";
+            }
+            Console.WriteLine(nextgreeting);
         }
     }
 }
